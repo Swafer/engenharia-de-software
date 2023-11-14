39d732a31defc1283213cdf7459973e0f9db6be9
Swafer <79225589+Swafer@users.noreply.github.com>
2023-11-13 22:26:41 -0300

    Delete colecaum directory

--- colecaum/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import*
-
-def carros(request):
-    carros = {
-         'carros':carro.objects.all()
-        }
-    return render(request, 'carros/carros.html', carros)
